d92e407834256efbc949bce47612b85cec950943
Abd-UlRahman <a.khalid5322@gmail.com>
2024-11-06 13:46:47 +0200

    new

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,8 +2,8 @@ function MainContent() {
     return (
         <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
         <h2 style={{ color: 'blue' }}>Barcelona, Spain</h2>
-        <p style={{ fontWeight: 'bold' }}>Here, vibrant colors and artistic masterpieces line the streets. Picture Gaudí’s surreal architecture, lively markets like La Boqueria, and golden Mediterranean beaches.</p>
-        <p> Barcelona offers a mix of laid-back beach vibes and bustling city life with rich, bold flavors in its food and culture.</p>
+        <p style={{ fontWeight: 'bold' }}>main, vibrant colors and artistic masterpieces line the streets. Picture Gaudí’s surreal architecture, lively markets like La Boqueria, and golden Mediterranean beaches.</p>
+        <p> I love to visit New York, Paris, and Tokyo.</p>
       </div>
     );
 }
