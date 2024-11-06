0aa5c27783a2bb9c2f83fcbce2bfaaee89910711
Abd-UlRahman <a.khalid5322@gmail.com>
2024-11-06 13:55:28 +0200

    making a count app

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,23 +1,13 @@
 import { useState } from "react";
 
-import WelcomeMessage from "./components/WelcomeMessage";
 
-import Header from "./components/Header";
-import UserProfile from "./components/UserProfile";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+import Counter from "./components/Counter";
 
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return(
-  <>
-  <Header/>
-  <UserProfile name="Abd-UlRahman" age="29" bio="Loves hiking and photography" />;
- <MainContent/>
- <Footer/>
-  </>)
+  return<Counter/>
 }
 
 export default App;
