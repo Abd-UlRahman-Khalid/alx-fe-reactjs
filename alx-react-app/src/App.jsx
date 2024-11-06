1093cb01a1a577bbe714f93f8d4c8d8ae49aecac
Abd-UlRahman <a.khalid5322@gmail.com>
2024-11-06 13:23:13 +0200

    adding UserProfile component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
 import WelcomeMessage from "./components/WelcomeMessage";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <WelcomeMessage />;
+  return<UserProfile name="Alice" age="25" bio="Loves hiking and photography" />;
 }
 
 export default App;
