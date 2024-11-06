f036df51cbfb352a1e5fc8f0325507fd7954e092
Abd-UlRahman <a.khalid5322@gmail.com>
2024-11-06 13:47:48 +0200

    footer

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -2,8 +2,8 @@ function Footer() {
     return (
         <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
         <h2 style={{ color: 'blue' }}>MainContent</h2>
-        <p>Age: <span style={{ fontWeight: 'bold' }}>MainContent</span></p>
-        <p>Bio: MainContent Bio</p>
+        <p>Age: <span style={{ fontWeight: 'bold' }}>Footer</span></p>
+        <p>Bio: footer</p>
       </div>
     );
 }
