906859039ac90c979f3c40f5c3674b9e8da0107b
Abd-UlRahman <a.khalid5322@gmail.com>
2024-11-11 07:08:26 +0200

    add Components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from "react";
 
 import WelcomeMessage from "./components/WelcomeMessage";
 import UserProfile from "./components/UserProfile";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MainContent from "./components/MainContent";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return;
   <>
+    <Header />
+    <MainContent />
     <WelcomeMessage />
-    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />;
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    <Footer />
   </>;
 }
 
