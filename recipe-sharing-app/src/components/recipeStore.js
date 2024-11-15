e429290bb89b7a219d6f84d97d3eb4930bb6ba94
Abd-UlRahman <a.khalid5322@gmail.com>
2024-11-15 07:20:20 +0200

    adding searchBar

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,9 +2,21 @@ import { create } from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: "",
+  filteredRecipes: [],
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
+
+  setSearchTerm: (term) => set({ searchTerm: term }),
+
+  // Filtering action that updates filteredRecipes based on searchTerm
+  filterRecipes: () =>
+    set((state) => ({
+      filteredRecipes: state.recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+      ),
+    })),
 
   // New action for deleting a recipe by ID
   deleteRecipe: (id) =>
