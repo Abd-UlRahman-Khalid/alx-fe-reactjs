6cc60673c46104753a905692a3b94d27fc10ea19
Abd-UlRahman <a.khalid5322@gmail.com>
2024-11-15 07:37:04 +0200

    add favorite list

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,6 +2,7 @@ import { create } from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+  favorites: [],
   searchTerm: "",
   filteredRecipes: [],
   addRecipe: (newRecipe) =>
@@ -18,6 +19,25 @@ const useRecipeStore = create((set) => ({
       ),
     })),
 
+  // Favorite
+  addFavorite: (recipeId) =>
+    set((state) => ({
+      favorites: [...state.favorites, recipeId],
+    })),
+  removeFavorite: (recipeId) =>
+    set((state) => ({
+      favorites: state.favorites.filter((id) => id !== recipeId),
+    })),
+
+  recommendations: [], // The list of recommended recipes based on the user's favorites
+  generateRecommendations: () =>
+    set((state) => {
+      // Mock implementation to generate recommendations based on favorite recipes
+      const recommended = state.recipes.filter(
+        (recipe) => state.favorites.includes(recipe.id) && Math.random() > 0.5
+      );
+      return { recommendations: recommended };
+    }),
   // New action for deleting a recipe by ID
   deleteRecipe: (id) =>
     set((state) => ({
