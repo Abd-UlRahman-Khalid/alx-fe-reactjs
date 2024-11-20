041e9aa1babe5469db8c9a2b1aca52005abdd82a
Abd-UlRahman <a.khalid5322@gmail.com>
2024-11-20 10:59:16 +0200

    put all files in components

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
@@ -7,22 +7,24 @@ import BlogPost from "./pages/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
-  const isAuthenticated = false; // Simulate authentication status
+  const isAuthenticated = false; // Simulated authentication status
 
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route
-        path="/profile/*"
-        element={
-          <ProtectedRoute isAuthenticated={isAuthenticated}>
-            <Profile />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="/login" element={<Login />} />
-      <Route path="/blog/:id" element={<BlogPost />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
