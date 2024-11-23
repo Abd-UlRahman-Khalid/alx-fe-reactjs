64e265f8055853953f9bcd0b998f1e96ed3a69cb
Abd-UlRahman <a.khalid5322@gmail.com>
2024-11-23 09:19:30 +0200

    adding App.test.js

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -3,8 +3,11 @@ import TodoList from "./components/TodoList";
 import "./App.css";
 
 const App = () => {
+  const [title, setTitle] = useState("Todo App");
+
   return (
     <div className="App">
+      <h1>Todo Application</h1>
       <TodoList />
     </div>
   );
