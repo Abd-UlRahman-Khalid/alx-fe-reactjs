0abfa06ad030cb62c451fce7edc2c98ddfb79b60
Abd-UlRahman <a.khalid5322@gmail.com>
2024-12-04 13:42:10 +0200

    make it responsive

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -15,7 +15,7 @@ const HomePage = () => {
       <h1 className="text-3xl font-bold text-center mb-6">
         Recipe Sharing Platform
       </h1>
-      <div className="flex justify-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6  align-items-center  justify-items-center">
         {recipes.map((recipe) => (
           <div
             key={recipe.id}
