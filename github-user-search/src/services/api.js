58f90bdbd8abe8f42cf6fcc2135b97ffe1defc3f
Abd-UlRahman <a.khalid5322@gmail.com>
2024-12-13 16:12:36 +0200

    changing the name of the variable

--- github-user-search/src/services/api.js
+++ github-user-search/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_GITHUB_API_BASE_URL,
+  baseURL: import.meta.env.REACT_APP_GITHUB_API_KEY,
 });
 
 export const searchGitHubUsers = (query) => api.get(`/search/users?q=${query}`);
